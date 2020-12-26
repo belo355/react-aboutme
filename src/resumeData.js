e87b19c35433aaa05a179ddef31bf3e1930e2a19
belo355 <edilson.belo@live.com>
2020-12-26 16:50:44 -0300

    init commit

---
+++ src/resumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Edilson Belo",
+    "role": "Developer SR",
+    "linkedinId":"https://www.linkedin.com/in/edilson-belo-143639106/",
+    "skypeid": "Edilson-belo",
+    "roleDescription": " ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/edilson-belo-143639106/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/belo355",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"Developer SR, MBA em Gestão de Projetos - FIAP",
+    "address":"Brazil, São Paulo",
+    "website":"edilson.belo@.live.com",
+    "education":[
+      {
+        "UniversityName":"UNIP - Universidade Paulista",
+        "specialization":"Análise e Dev. de Sistemas",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2015",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"SENAC",
+        "specialization":"Engenharia da Qualidade de Software",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2019",
+        "Achievements":""
+      }, 
+      {
+        "UniversityName":"FIAP",
+        "specialization":"Gestão da Tecnologia da Informação",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2020",
+        "Achievements":""
+      }, 
+      {
+        "UniversityName":"Scrum.org",
+        "specialization":"Professional Scrum Master I",
+        "MonthOfPassing":"Ago",
+        "YearOfPassing":"2019",
+        "Achievements":""
+      }, 
+      {
+        "UniversityName":"EXIN",
+        "specialization":"DevOps Professional",
+        "MonthOfPassing":"Ago",
+        "YearOfPassing":"2020",
+        "Achievements":""
+      }, 
+    ],
+    "work":[
+      {
+        "CompanyName":"Santander Tecnologia",
+        "specialization":"Developer SR",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2012",
+        "Achievements":"System Team - DevOps, Scrum Master, Developer Sr"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"JAVA"
+      },
+      {
+        "skillname":"JAVASCRIPT"
+      },
+      {
+        "skillname":"Agil"
+      }
+    ],
+    "portfolio":[
+      // {
+      //   "name":"project1",
+      //   "description":"mobileapp",
+      //   "imgurl":"images/portfolio/phone.jpg"
+      // },
+     
+    ],
+    "testimonials":[
+      {
+        "description":"Edilson é um profissional com muita facilidade de se adaptar, muita dedicação e determinação. Mudou de área e em pouco tempo passou a ser uma das referências na área e no sistema que atua, assumindo responsabilidades de membros com mais tempo de área.Comprometido com as entregas e o principal, segue muito bem a estratégia de Customer Centric.Edilson obrigado pela dedicação e comprometimento!",
+        "name":"Rodrigo Perestrelo"
+      }
+    ]
+  }
+  
+  export default resumeData
